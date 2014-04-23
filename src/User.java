8093dfc1d495ffc87f207416b7805f53c3cef1ad
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:15:54 +0300

    Create User.java

---
+++ src/User.java
@@ -0,0 +1,53 @@
+import java.util.Vector;
+
+public class User {
+
+  public String Login;
+
+  public Integer password;
+
+  public Integer k1;
+
+  public Integer k2;
+
+  public Integer k3;
+
+  public Integer k4;
+
+  public Integer k5;
+
+  public Integer k6;
+
+  public Integer k7;
+
+  public Integer k8;
+
+  public Integer k9;
+
+  public Integer k10;
+
+  public Integer n1;
+
+  public Integer n2;
+
+  public Integer n3;
+
+  public Integer n4;
+
+  public Integer n5;
+
+  public Integer n6;
+
+    public Vector  myAOne-timeRequest;
+        public Vector  myHistory;
+
+  public void GetRegistration() {
+  }
+
+  public void logIn() {
+  }
+
+  public void changeInformation() {
+  }
+
+}
