a53ae487f5fabea812e17dd7edd66b98cc3a8438
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:08:29 +0300

    Create AOne-timeRequest.java

---
+++ src/AOne-timeRequest.java
@@ -0,0 +1,32 @@
+import java.util.Vector;
+
+public class AOne-timeRequest extends User {
+
+  public Integer typeT;
+
+  public Integer typeP;
+
+  public Integer typeF;
+
+  public Integer r;
+
+  public Integer rK;
+
+  public Integer price;
+
+  public Integer priceK;
+
+  public Integer markK;
+
+  public Integer place;
+
+    public Vector  myUser;
+    public Vector  myUser;
+    
+  public void searchPlace() {
+  }
+
+  public void resetInformation() {
+  }
+
+}
