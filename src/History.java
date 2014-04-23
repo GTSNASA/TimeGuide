373af7197cd90be1a8434465fd28ff6b91dacfea
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:09:28 +0300

    Create History.java

---
+++ src/History.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class History {
+
+  public Integer visit;
+
+  public Integer place;
+
+    public Vector  myPlacesList;
+    public Vector  myUser;
+    public Vector  myPlacesList;
+
+  public void storeNumVisit() {
+  }
+
+  public void addVisit() {
+  }
+
+  public void newOperation() {
+  }
+
+}
