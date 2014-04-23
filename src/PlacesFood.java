f6a296f915b2b4328661446e5b4f05a24ab4e9b4
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:13:49 +0300

    Create PlacesFood.java

---
+++ src/PlacesFood.java
@@ -0,0 +1,24 @@
+import java.util.Vector;
+
+public class PlacesFood extends Places {
+
+  public Integer typeT;
+
+  public Integer typeP;
+
+  public Integer typeF;
+
+  public Integer x;
+
+  public Integer y;
+
+  public Integer price;
+
+  public Integer mark;
+
+    public Vector  myPlaces;
+
+  public void storePlace() {
+  }
+
+}
