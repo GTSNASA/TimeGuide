5a78a2f60b0ec6b154cc49ca1ef8b6d2761302c7
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:12:40 +0300

    Create PlacesEntertainment.java

---
+++ src/PlacesEntertainment.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class PlacesEntertainment extends Places {
+
+  public Integer typeT;
+
+  public Integer typeP;
+
+  public Integer x;
+
+  public Integer y;
+
+  public Integer price;
+
+  public Integer mark;
+
+    public Vector  myPlaces;
+
+  public void storePlace() {
+  }
+
+}
