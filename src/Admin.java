b35c6f12b32ef552e4a4cc50a42577180f458c64
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:06:55 +0300

    Create Admin.java

---
+++ src/Admin.java
@@ -0,0 +1,17 @@
++public class Admin {
++
++  public Integer place;
++
++ public void addPlace() {
++  }
++
++  public void changeInformationPlace() {
++  }
++
++  public void deletePlace() {
++  }
++
++  public void searchPlace() {
++  }
++
++}
