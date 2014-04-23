3f8c849d5b2c4d171c441ab0b4296f47da6a0770
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:15:10 +0300

    Create PlacesList.java

---
+++ src/PlacesList.java
@@ -0,0 +1,31 @@
+import java.util.Vector;
+
+public class PlacesList {
+
+  public Integer place;
+
+    public Vector  myUser;
+    public Vector  myAOne-timeRequest;
+        public Vector  myPlacesList;
+    public Vector  myPlacesList;
+    public Vector  myHistory;
+  
+  public void showList() {
+  }
+
+  public void showInformation() {
+  }
+
+  public void sortPlaces() {
+  }
+
+  public void calculateCoefficient() {
+  }
+
+  public void getPlace() {
+  }
+
+  public void calcDistance() {
+  }
+
+}
