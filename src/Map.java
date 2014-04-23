51105512ffaa907645137d7e52a9174c370ffeb5
Giryavec <Grizvald95@gmail.com>
2014-04-24 00:10:57 +0300

    Create Map.java

---
+++ src/Map.java
@@ -0,0 +1,24 @@
+import java.util.Vector;
+
+public class Map {
+
+  public Integer x;
+
+  public Integer y;
+
+    public Vector  myPlacesList;
+    public Vector  myAOne-timeRequest;
+
+  public void markPlace() {
+  }
+
+  public void showMap() {
+  }
+
+  public void showMe() {
+  }
+
+  public void showPlace() {
+  }
+
+}
