264d495d99b7c228e53f0156a7537185f05c6bd2
Giryavec <Grizvald95@gmail.com>
2014-04-27 21:01:06 +0300

    Update and rename Mark.java to MarkOfPlace.java

---
+++ src/MarkOfPlace.java
@@ -0,0 +1,22 @@
+package timeSpending;
+
+import java.util.Vector;
+
+public class MarkOfPlace< E> {
+
+  public Integer markUser;
+
+  public Integer id;
+
+    public Vector< E>  myPlacesList;
+
+  public void estimatePlace() {
+  }
+
+  public void calcNewMark() {
+  }
+
+  public void saveMark() {
+  }
+
+}
