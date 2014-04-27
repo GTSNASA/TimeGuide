9727698253c52a93643936884e6fe035112796dd
Giryavec <Grizvald95@gmail.com>
2014-04-27 20:56:29 +0300

    Update and rename AOne-timeRequest.java to AOneTimeRequest.java

---
+++ src/AOneTimeRequest.java
@@ -0,0 +1,31 @@
+package timeSpending;
+import java.util.Vector;
+
+public class AOneTimeRequest<E> extends User {
+
+  public String typeTimespending;
+
+  public String typePlace;
+
+  public String typeFood;
+
+  public Integer distance;
+
+  public Integer markDistance;
+
+  public Integer price;
+
+  public Integer markPrice;
+
+  public Integer markMark;
+
+    public Vector< E>  myUser;
+
+    
+  public void searchPlace() {
+  }
+
+  public void resetInformation() {
+  }
+
+}
