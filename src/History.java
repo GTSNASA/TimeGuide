1f99720634379344ce7849e6199d78f3295a20a4
MarinaKrivosheyenko <marina.krivosheyenko@ukr.net>
2014-05-20 22:59:38 +0300

    Update History.java

--- src/History.java
+++ src/History.java
@@ -2,20 +2,49 @@ package timeSpending;
 
 import java.util.Vector;
 
-public class History<E> {
+public class History {
 
-  public Integer visit;
+	  private Integer visit;
 
-  public Integer id;
+	  private Integer id;
 
-    public Vector< E>  myPlacesList;
-
-
-  public void storeNumVisit() {
-  }
-
-  public void addVisit() {
-  }
+	    private Vector< String>  myPlacesList;
 
 
-}
+	  public void storeNumVisit() {
+	  }
+
+	  public Integer addVisit(User user) {
+		  setVisit( getVisit() + 1);
+		  return getVisit();
+		  
+	  }
+
+
+	    // GETTER & SETTER
+
+	    public Integer getVisit() {
+	        return visit;
+	    }
+
+	    public void setVisit(Integer visit) {
+	        this.visit = visit;
+	    }
+
+	    public Integer getId() {
+	        return id;
+	    }
+
+	    public void setId(Integer id) {
+	        this.id = id;
+	    }
+
+	    public Vector<String> getMyPlacesList() {
+	        return myPlacesList;
+	    }
+
+	    public void setMyPlacesList(Vector<String> myPlacesList) {
+	        this.myPlacesList = myPlacesList;
+	    }
+	    ///////////////////////////////
+	}
