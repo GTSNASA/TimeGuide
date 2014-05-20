b6ef0f37fc71014697c5e5c3d2397fbe5bea72fe
MarinaKrivosheyenko <marina.krivosheyenko@ukr.net>
2014-05-20 23:09:35 +0300

    Create PlacesEntertainmentInterface.java

---
+++ src/PlacesEntertainmentInterface.java
@@ -0,0 +1,7 @@
+package timeSpending;
+
+public interface PlacesEntertainmentInterface {
+	 public String getTimeSpendingInterval() ;
+	 public void setTimeSpendingInterval(String timeSpendingInterval);
+
+}
