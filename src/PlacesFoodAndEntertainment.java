95120aece87c27d8323a9db4ee5eeca5e8d73455
MarinaKrivosheyenko <marina.krivosheyenko@ukr.net>
2014-05-20 23:04:12 +0300

    Update PlacesFoodAndEntertainment.java

--- src/PlacesFoodAndEntertainment.java
+++ src/PlacesFoodAndEntertainment.java
@@ -1,7 +1,41 @@
 package timeSpending;
 
-public class PlacesFoodAndEntertainment extends PlacesFood, PlacesEntertainment {
+public class PlacesFoodAndEntertainment extends PlacesFood implements PlacesEntertainmentInterface {
 
-	  public String nameOfTypeUnionPlaces;
 
+
+	  public PlacesFoodAndEntertainment(Integer x, Integer y, Integer price,
+			Integer mark, String typePlace, String nameOfPlace,
+			String addressOfPlace, Integer id) {
+		super(x, y, price, mark, typePlace, nameOfPlace, addressOfPlace, id);
+		// TODO Auto-generated constructor stub
 	}
+
+	private String nameOfTypeUnionPlaces;
+
+    //GETTER & SETTER
+
+
+    public String getNameOfTypeUnionPlaces() {
+        return nameOfTypeUnionPlaces;
+    }
+
+    public void setNameOfTypeUnionPlaces(String nameOfTypeUnionPlaces) {
+        this.nameOfTypeUnionPlaces = nameOfTypeUnionPlaces;
+    }
+    /////////////////
+
+	@Override
+	public String getTimeSpendingInterval() {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTimeSpendingInterval(String timeSpendingInterval/*, PlacesEntertainment placesE*/) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
