9fbbd3b097f663b8c4b6723552cba92e43b3b310
MarinaKrivosheyenko <marina.krivosheyenko@ukr.net>
2014-05-20 23:03:41 +0300

    Update PlacesFood.java

--- src/PlacesFood.java
+++ src/PlacesFood.java
@@ -2,9 +2,28 @@ package timeSpending;
 
 public class PlacesFood extends Places {
 
-	  public String typeFood;
+	// super for extends
 
-	  public void storePlace() {
-	  }
+   public PlacesFood(Integer x, Integer y, Integer price, Integer mark, String typePlace, String nameOfPlace, String addressOfPlace, Integer id) {
+        super(x, y, price, mark, typePlace, nameOfPlace, addressOfPlace, id, id);
+    }
 
-	}
+    private String typeFood;
+
+
+    //GETTER & SETTER
+
+    public String getTypeFood() {
+        return typeFood;
+    }
+
+    public void setTypeFood(String typeFood) {
+        this.typeFood = typeFood;
+    }
+    ///////////////
+}
+
+	  
+
+	  
+	  
