b02752f16f7531df5c79c61ee97d5079729f7510
MarinaKrivosheyenko <marina.krivosheyenko@ukr.net>
2014-05-20 22:57:37 +0300

    Update AOneTimeRequest.java

--- src/AOneTimeRequest.java
+++ src/AOneTimeRequest.java
@@ -1,31 +1,175 @@
 package timeSpending;
-import java.util.Vector;
+//import java.util.Vector;
 
-public class AOneTimeRequest<E> extends User {
+public class AOneTimeRequest extends User {
 
-  public String typeTimespending;
+	  private String typeTimespending;
 
-  public String typePlace;
+	  private String typePlace;
 
-  public String typeFood;
+	  private String typeFood;
 
-  public Integer distance;
+	  private Integer distance;
 
-  public Integer markDistance;
+	  private Integer markDistance;
 
-  public Integer price;
+	  private Integer price;
 
-  public Integer markPrice;
+	  private Integer markPrice;
 
-  public Integer markMark;
+	  private Integer markMark;
+	  
+	  private Integer pointX = 3;
+	  
+	  private Integer 	pointY = 3;
+	  
+	   
 
-    public Vector< E>  myUser;
+	
+	  public void searchPlace(String typeT, String typeP, Integer distan, Integer pric, Integer mm, Integer mp, Integer md, String tf, Integer x, Integer y) {
+		 /* String typeT="Food";
+		  String typeP="Bar";
+		  Integer distan=400;
+		  Integer pric = 43;
+		  Integer mm = 8;
+		  Integer mp = 7;
+		  Integer md = 4;
+		  String tf = "Mecixan";
+		  Integer x = 44;
+		  Integer y = 22;*/
+		  setTypeTimespending(typeT);
+		  setTypePlace(typeP);
+		  setDistance(distan);
+		  setPrice(pric);
+		  setMarkMark(mm);
+		  setMarkPrice(mp);
+		  setMarkDistance(md);
+		  setTypeFood(tf);
+		  setPointX(x);
+		  setPointY(y);
+		 // map.showMe();
+	  }
 
-    
-  public void searchPlace() {
-  }
+	  public void resetInformation() {
+		  setTypeTimespending(null);
+		  setTypePlace(null);
+		  setDistance(null);
+		  setPrice(null);
+		  setMarkMark(null);
+		  setMarkPrice(null);
+		  setMarkDistance(null);
+		  setTypeFood(null);
+		  setPointX(null);
+		  setPointY(null);
+		  
+	  }
 
-  public void resetInformation() {
-  }
 
-}
+	    public AOneTimeRequest(String typeTimespending, String typePlace,
+			String typeFood, Integer distance, Integer markDistance,
+			Integer price, Integer markPrice, Integer markMark, Integer pointX,
+			Integer pointY) {
+		super();
+		this.typeTimespending = typeTimespending;
+		this.typePlace = typePlace;
+		this.typeFood = typeFood;
+		this.distance = distance;
+		this.markDistance = markDistance;
+		this.price = price;
+		this.markPrice = markPrice;
+		this.markMark = markMark;
+		this.pointX = pointX;
+		this.pointY = pointY;
+		
+	}
+
+		// GETTER & SETTER
+
+	    public String getTypeTimespending() {
+	        return typeTimespending;
+	    }
+
+	    public void setTypeTimespending(String typeTimespending) {
+	        this.typeTimespending = typeTimespending;
+	    }
+
+	    public String getTypePlace() {
+	        return typePlace;
+	    }
+
+	    public void setTypePlace(String typePlace) {
+	        this.typePlace = typePlace;
+	    }
+
+	    public Integer getDistance() {
+	        return distance;
+	    }
+
+	    public void setDistance(Integer distance) {
+	        this.distance = distance;
+	    }
+
+	    public Integer getPrice() {
+	        return price;
+	    }
+
+	    public void setPrice(Integer price) {
+	        this.price = price;
+	    }
+
+	    public Integer getMarkMark() {
+	        return markMark;
+	    }
+
+	    public void setMarkMark(Integer markMark) {
+	        this.markMark = markMark;
+	    }
+
+	    public Integer getMarkPrice() {
+	        return markPrice;
+	    }
+
+	    public void setMarkPrice(Integer markPrice) {
+	        this.markPrice = markPrice;
+	    }
+
+	    public Integer getMarkDistance() {
+	        return markDistance;
+	    }
+
+	    public void setMarkDistance(Integer markDistance) {
+	        this.markDistance = markDistance;
+	    }
+
+	    public String getTypeFood() {
+	        return typeFood;
+	    }
+
+	    public void setTypeFood(String typeFood) {
+	        this.typeFood = typeFood;
+	    }
+	/* /////////////////////// */
+
+
+
+		public Integer getPointX() {
+			return pointX;
+		}
+
+		public void setPointX(Integer pointX) {
+			this.pointX = pointX;
+		}
+
+		public Integer getPointY() {
+			return pointY;
+		}
+
+		public void setPointY(Integer pointY) {
+			this.pointY = pointY;
+		}
+
+
+
+	}
+
+
